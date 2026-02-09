e5f6c05e424d49b10fe50f7c6b6fc97e34c78f84
Gerardo6741 <759369@student.dusd.net>
2026-02-09 14:16:54 -0800

    2w
    wwww

---
+++ P7TellezScriptingProject/Assets/Scripts/LinearInter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float result = Mathf.Lerp(3f, 5f, 0.5f);
+        Vector3 from = new Vector3(1f, 2f, 3f);
+        Vector3 to = new Vector3(5f, 6f, 7f);
+
+        // Here result = (4, 5, 6)
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
